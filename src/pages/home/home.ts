9d7fe228691a87ba0efbd2d5286f6e7e5e280a23
Rodrigo Bossini <professorbossini@gmail.com>
2019-10-25 15:52:25 -0300

    A gente chegou até o passo 14 do pdf.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  
+  notes = [
+    {
+      id: 1,
+      date: '2016-09-15',
+      title: 'Firebase',
+      content: 'Firebase permite o desenvolvimento serverless'   
+    },
+    {
+      id: 2,
+      date: '2019-07-12',
+      title: "Ionic",
+      content: "Ionic para desenvolvimento híbrido"  
+    },
+    {
+      id: 3,
+      title: "Angular",
+      content: "Já está na versão 8",
+      date: "2019-10-25"
+    }
+  ]
+
+
+  //injeção de dependência
+  //navCtrl = new NavController();
+  //é um tipo de inversão de controle
   constructor(public navCtrl: NavController) {
+    
+  }
+
+  onItemClick (note){
+    //console.log ("onItemClick", note);
+    this.navCtrl.push ('DetailPage', {noteParam: note});
 
   }
 
